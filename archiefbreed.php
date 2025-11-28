2538c72475f696bf0f556c9485507d3b55fb9991
irisxg <irisgovaard@gmail.com>
2025-11-28 11:20:45 +0100

    bijna alles af graag van de read me af blijven daar staat code in wat ik later nodig heb

--- archiefbreed.php
+++ archiefbreed.php
@@ -1,106 +1,45 @@
 <!DOCTYPE html>
 <html lang="nl">
-
 <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Panorama</title>
-     <link rel="stylesheet" href="./assets/css/style.css" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Panorama</title>
+  <link rel="stylesheet" href="./assets/css/style.css" />
 </head>
-
 <body>
-     <h1></h1>
-     <main>
-          <div class="panorama">
-               <img src="./assets/img/pannoramafoto2.jpg" alt="Panorama 2"
-                    style="height: 500px; z-index: 1; margin-left: 0px; margin-top: 0px;">
-
-               <img src="./assets/img/pannoramafoto3.jpg" alt="Panorama 3"
-                    style="height: 500px; z-index: 2; margin-left: -38px; margin-top: -3px;">
-
-               <img src="./assets/img/pannoramafoto4.jpg" alt="Panorama 4"
-                    style="height: 497.5px; z-index: 3; margin-left: -40px; margin-top: -3px;">
-
-               <img src="./assets/img/pannoramafoto5.jpg" alt="Panorama 5"
-                    style="height: 500px; z-index: 4; margin-left: -55px; margin-top: -3px;">
-
-               <img src="./assets/img/pannoramafoto6.jpg" alt="Panorama 6"
-                    style="height: 506px; z-index: 5; margin-left: -56px; margin-top: -8px;">
-
-               <img src="./assets/img/pannoramafoto7.jpg" alt="Panorama 7"
-                    style="height: 511px; z-index: 6; margin-left: -60px; margin-top: -4px;">
-
-               <img src="./assets/img/pannoramafoto8.jpg" alt="Panorama 8"
-                    style="height: 523px; z-index: 5; margin-left: -46px; margin-top: -28px;">
-
-               <img src="./assets/img/pannoramafoto9.jpg" alt="Panorama 9"
-                    style="height: 502px; z-index: 5; margin-left: -44px; margin-top: -6px;">
-
-               <img src="./assets/img/pannoramafoto10.jpg" alt="Panorama 10"
-                    style="height: 514px; z-index: 9; margin-left: -43px; margin-top: -19px;">
-
-               <img src="./assets/img/pannoramafoto11.jpg" alt="Panorama 11"
-                    style="height: 511px; z-index: 10; margin-left: -44px; margin-top: -15px;">
-
-               <img src="./assets/img/pannoramafoto12.jpg" alt="Panorama 12"
-                    style="height: 515px; z-index: 11; margin-left: -52px; margin-top: -14px;">
-
-               <img src="./assets/img/pannoramafoto13.jpg" alt="Panorama 13"
-                    style="height: 518px; z-index: 12; margin-left: -39px; margin-top: -19px;">
-
-               <img src="./assets/img/pannoramafoto14.jpg" alt="Panorama 14"
-                    style="height: 515.5px; z-index: 13; margin-left: -48px; margin-top: -17px;">
-
-               <img src="./assets/img/pannoramafoto15.jpg" alt="Panorama 15"
-                    style="height: 509px; z-index: 14; margin-left: -58px; margin-top: -12px;">
-
-               <img src="./assets/img/pannoramafoto16.jpg" alt="Panorama 16"
-                    style="height: 506px; z-index: 15; margin-left: -59px; margin-top: -5px;">
-
-               <img src="./assets/img/pannoramafoto17.jpg" alt="Panorama 17"
-                    style="height: 505px; z-index: 16; margin-left: -37px; margin-top: -1px;">
-
-               <img src="./assets/img/pannoramafoto18.jpg" alt="Panorama 18"
-                    style="height: 508px; z-index: 17; margin-left: -36px; margin-top: 3px;">
-
-               <img src="./assets/img/pannoramafoto19.jpg" alt="Panorama 19"
-                    style="height: 515px; z-index: 18; margin-left: -40px; margin-top: 1.5px;">
-
-               <img src="./assets/img/pannoramafoto20.jpg" alt="Panorama 20"
-                    style="height: 526px; z-index: 19; margin-left: -37px; margin-top: -5px;">
-
-               <img src="./assets/img/pannoramafoto21.jpg" alt="Panorama 21"
-                    style="height: 534px; z-index: 18; margin-left: -32px; margin-top: -6px;">
-
-               <img src="./assets/img/pannoramafoto22.jpg" alt="Panorama 22"
-                    style="height: 526px; z-index: 18; margin-left: -44px; margin-top: -3px;">
-
-               <img src="./assets/img/pannoramafoto23.jpg" alt="Panorama 23"
-                    style="height: 542px; z-index: 22; margin-left: -39px; margin-top: -15px;">
-
-               <img src="./assets/img/pannoramafoto24.jpg" alt="Panorama 24"
-                    style="height: 528px; z-index: 23; margin-left: -40px; margin-top: 2px;">
-
-               <img src="./assets/img/pannoramafoto25.jpg" alt="Panorama 25"
-                    style="height: 506px; z-index: 24; margin-left: -34px; margin-top: 16px;">
-
-               <img src="./assets/img/pannoramafoto26.jpg" alt="Panorama 26"
-                    style="height: 524px; z-index: 25; margin-left: -30px; margin-top: 1px;">
-
-               <img src="./assets/img/pannoramafoto27.jpg" alt="Panorama 27"
-                    style="height: 510.5px; z-index: 26; margin-left: -35px; margin-top: 12px;">
-
-               <img src="./assets/img/pannoramafoto28.jpg" alt="Panorama 28"
-                    style="height: 527px; z-index: 27; margin-left: -42px; margin-top: 5px;">
-
-               <img src="./assets/img/pannoramafoto29.jpg" alt="Panorama 29"
-                    style="height: 540px; z-index: 28; margin-left: -48px; margin-top: -4px;">
-
-               <img src="./assets/img/pannoramafoto30.jpg" alt="Panorama 30"
-                    style="height: 534px; z-index: 29; margin-left: -44px; margin-top: -1px;">
-
-          </div>
-     </main>
+  <main>
+    <div class="panorama">
+      <img src="./assets/img/1.jpg" alt="Panorama 1">
+      <img src="./assets/img/2.jpg" alt="Panorama 2">
+      <img src="./assets/img/3.jpg" alt="Panorama 3">
+      <img src="./assets/img/4.jpg" alt="Panorama 4">
+      <img src="./assets/img/5.jpg" alt="Panorama 5">
+      <img src="./assets/img/6.jpg" alt="Panorama 6">
+      <img src="./assets/img/7.jpg" alt="Panorama 7">
+      <img src="./assets/img/8.jpg" alt="Panorama 8">
+      <img src="./assets/img/9.jpg" alt="Panorama 9">
+      <img src="./assets/img/10.jpg" alt="Panorama 10">
+      <img src="./assets/img/11.jpg" alt="Panorama 11">
+      <img src="./assets/img/12.jpg" alt="Panorama 12">
+      <img src="./assets/img/13.jpg" alt="Panorama 13">
+      <img src="./assets/img/14.jpg" alt="Panorama 14">
+      <img src="./assets/img/15.jpg" alt="Panorama 15">
+      <img src="./assets/img/16.jpg" alt="Panorama 16">
+      <img src="./assets/img/17.jpg" alt="Panorama 17">
+      <img src="./assets/img/18.jpg" alt="Panorama 18">
+      <img src="./assets/img/19.jpg" alt="Panorama 19">
+      <img src="./assets/img/20.jpg" alt="Panorama 20">
+      <img src="./assets/img/21.jpg" alt="Panorama 21">
+      <img src="./assets/img/22.jpg" alt="Panorama 22">
+      <img src="./assets/img/23.jpg" alt="Panorama 23">
+      <img src="./assets/img/24.jpg" alt="Panorama 24">
+      <img src="./assets/img/25.jpg" alt="Panorama 25">
+      <img src="./assets/img/26.jpg" alt="Panorama 26">
+      <img src="./assets/img/27.jpg" alt="Panorama 27">
+      <img src="./assets/img/28.jpg" alt="Panorama 28">
+      <img src="./assets/img/29.jpg" alt="Panorama 29">
+      <img src="./assets/img/30.jpg" alt="Panorama 30">
+    </div>
+  </main>
 </body>
-
 </html>
---
+++ assets/js/panorama.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.hotspot').forEach(hotspot => {
+    hotspot.addEventListener('click', () => {
+      const wrapper = hotspot.closest('.image-wrapper');
+      const infoBox = wrapper.querySelector('.info-box');
+      infoBox.style.display = infoBox.style.display === 'block' ? 'none' : 'block';
+    });
+  });
+  
